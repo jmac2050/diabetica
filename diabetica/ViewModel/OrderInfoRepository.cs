27c27d0d0f79d5b2bea9e14bb1a5a40353a02b01
Jürgen Maczioch <juergen.maczioch@outlook.de>
2019-02-21 21:22:24 +0100

    201902212122 Diverse Anpassungen

--- diabetica/ViewModel/OrderInfoRepository.cs
+++ diabetica/ViewModel/OrderInfoRepository.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using CsvHelper;
 using diabetica.Model;
 
 namespace diabetica.ViewModel
@@ -20,9 +24,9 @@ namespace diabetica.ViewModel
 		}
 
 		private void GenerateDiabetumGridContent ()
-		{
-		    OrderInfoCollection.Add(new OrderInfo("  0", "<", " 60", " 0", " 0", " 0", " 0"));
-		    OrderInfoCollection.Add(new OrderInfo(" 60", "<", " 90", " 0", " 0", " 0", " 0"));
+        {         
+            OrderInfoCollection.Add(new OrderInfo("  0", "<", " 60", " 0", " 0", " 0", " 0"));
+            OrderInfoCollection.Add(new OrderInfo(" 60", "<", " 90", " 0", " 0", " 0", " 0"));
 		    OrderInfoCollection.Add(new OrderInfo(" 90", "<", "120", "13", " 6", " 8", " 0"));
 		    OrderInfoCollection.Add(new OrderInfo("120", "<", "150", "13", " 6", " 8", " 0"));
 		    OrderInfoCollection.Add(new OrderInfo("150", "<", "180", "14", " 7", " 9", " 0"));
@@ -31,7 +35,7 @@ namespace diabetica.ViewModel
 		    OrderInfoCollection.Add(new OrderInfo("240", "<", "270", "17", "10", "12", " 2"));
 		    OrderInfoCollection.Add(new OrderInfo("270", "<", "300", "18", "11", "13", " 4"));
 		    OrderInfoCollection.Add(new OrderInfo("300", "<", "900", "19", "12", "14", " 4"));
-        }
-	}
+        }     
+    }
 }
 
